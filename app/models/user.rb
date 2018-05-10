5fe8e00c46fcbda675c8404842bf865412b073f2
dannamae <dannamaeiso@gmail.com>
2018-05-10 08:34:14 +0800

    changed students to users

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+
+  validates :studentnum, presence: true
+  validates :firstname, presence: true
+  validates :middlename, presence: true
+  validates :lastname, presence: true
+  validates :sectionname, presence: true
+  validates :password, presence: true
+end
