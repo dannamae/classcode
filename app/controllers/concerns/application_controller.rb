e661ce6a88f9f66c21065489c859196887290701
dannamae <dannamaeiso@gmail.com>
2018-05-16 13:05:36 +0800

    Application

--- app/controllers/concerns/application_controller.rb
+++ app/controllers/concerns/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
-end
+
+   attr_reader :current_user
+   private
+
+
+   def authenticate_user!
+     _, token,  password = request.headers['Authorization'].split(' ')
+     user = User.where("password = ? and tokens like ?", email, "%#{token}%").first
+     if user.present? && user.has_valid_token?(token)
+       @current_user = user
+     else
+       render json: {error: 'Invalid Authorization token'}, status: :unauthorized
+     end
+   end
+
+ end
