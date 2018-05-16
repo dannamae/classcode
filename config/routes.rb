556ca693c53cdf89e8da420f1d63a97451486941
dannamae <dannamaeiso@gmail.com>
2018-05-16 11:10:30 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,23 @@
 Rails.application.routes.draw do
+  get 'welcome/homepage'
+  get 'welcome/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   namespace 'api' do
     namespace 'v1' do
       resources :admins
       resources :users
       resources :attendances
       resources :sections
-
     end
   end
+
+  resources :homepage
+  resources :new
+
+  post 'session', to: 'session#create', as: 'login'
+  delete 'session', to: 'session#destroy', as: 'logout'
+
+
 end
