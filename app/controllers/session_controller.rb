393a047cdf3dbcd6761e114febfec35d13ae46ad
dannamae <dannamaeiso@gmail.com>
2018-05-16 13:06:15 +0800

    SESSION

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,9 +1,18 @@
 class SessionController < ApplicationController
 
+
   def create
   user = User.find_by(studentnum: params[:studentnum])
 
   if user && user.authenticate(params[:password])
-    render json: { }, status: :ok
-  end 
+      # token = user.generate_token
+    render json: {status: 'Sucess', data:user}, status: :ok
+else
+  render json: { error: "Wrong username or password" }, status: :unauthorized
+end
+end
+
+def destroy
+  current_user = nil
+end
 end
