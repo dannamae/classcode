869131989b1195e6414faab32bfd2271639b2266
cmllbs <bosecamille@gmail.com>
2018-05-17 11:03:50 +0800

    Authtoken

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,18 +1,21 @@
 class SessionController < ApplicationController
 
+  def current_user
+    @current_user ||= authentication_auth_token
+  end
 
   def create
   user = User.find_by(studentnum: params[:studentnum])
 
   if user && user.authenticate(params[:password])
-      # token = user.generate_token
+    user.regenerate_auth_token
     render json: {status: 'Sucess', data:user}, status: :ok
-else
-  render json: { error: "Wrong username or password" }, status: :unauthorized
-end
+  else
+    render json: { error: "Wrong username or password" }, status: :unauthorized
+  end
 end
 
-def destroy
-  current_user = nil
-end
+  def destroy
+    current_user = nil
+  end
 end
