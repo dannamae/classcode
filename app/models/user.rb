444426c6681d5adf012d6238bcf2e5c30268c304
dannamae <dannamaeiso@gmail.com>
2018-05-17 12:59:51 +0800

    user. edited.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :studentnum, presence: true
   validates :firstname, presence: true
-#  validates :middlename, presence: true
+   validates :middlename, presence: true
   validates :lastname, presence: true
   validates :sectionname, presence: true
   validates :password_digest, presence: true
