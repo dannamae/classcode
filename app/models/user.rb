cbff7487f47f69daa0e2e4fa8202860968ad80b7
dannamae <dannamaeiso@gmail.com>
2018-05-22 16:51:48 +0800

    User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_secure_token :auth_token
   validates_uniqueness_of :studentnum
 
+
   validates :studentnum, presence: true
   validates :firstname, presence: true
    validates :middlename, presence: true
